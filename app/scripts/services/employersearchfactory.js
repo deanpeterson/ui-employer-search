375d2efdcecd26852b05cd27c2575c4cf589cd06
deanpeterson <peterson.dean@gmail.com>
2014-07-15 11:37:13 -0500

    added factory

---
+++ app/scripts/services/employersearchfactory.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name employerSearchApp.employerSearchFactory
+ * @description
+ * # employerSearchFactory
+ * Factory in the employerSearchApp.
+ */
+angular.module('employerSearchApp')
+  .factory('employerSearchFactory', function () {
+    // Service logic
+    // ...
+
+    var meaningOfLife = 42;
+
+        var defaults = [];
+
+        var service = {
+
+            employers : [],
+            save : function() {
+                localStorage.presently = angular
+                    .toJson(service.employers);
+            },
+            restore : function() {
+                service.employers = angular
+                    .fromJson(localStorage.presently)
+                    || defaults;
+                return service.employers;
+            }
+        };
+        return service;
+  });

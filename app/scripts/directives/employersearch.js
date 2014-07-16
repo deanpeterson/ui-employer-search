8b9f23da69233214c825477964ba83effe35f057
deanpeterson <peterson.dean@gmail.com>
2014-07-16 13:22:39 -0500

    made portable

--- app/scripts/directives/employersearch.js
+++ app/scripts/directives/employersearch.js
@@ -7,57 +7,70 @@
  * # employerSearch
  */
 angular.module('directives.employerSearch', [])
-  .directive('employerSearch', function () {
-    return {
-      templateUrl: function(tElement,tAttrs){
-          return tAttrs.templateUrl;
-      },
-      restrict: 'EAC',
-      link: function postLink(scope, element, attrs) {
-        var select2Element = element.children().get(0);
-        $(select2Element).select2({
-            minimumInputLength: attrs.minimumInputLength,
-            maximumSelectionSize:attrs.maximumSelectionSize,
-            multiple:true,
-            id:function(obj){
-              return obj.id;
-            },
-            ajax:{
-              url : attrs.employerSearchServiceUrl,
-              transport:function(queryParams){
-                  $.ajax({
-                      url: queryParams.url+'?q='+queryParams.data.q,
-                      async:false,
-                      headers: { 'Authorization': 'Bearer ' + keycloak.token }
-                  }).done(function(results){
-                      queryParams.success({results:results});
-                  });
+    .directive('employerSearch', function ($compile, $http, $templateCache) {
+        return {
+            restrict: 'EAC',
+            replace: true,
+            link: function postLink(scope, element, attrs) {
 
-              },
-              dataType: 'jsonp',
-              data : function(term, page){
-                  return {q:term} //search term
-              },
-              results:function(data,page,query)
-              {
-                  return data;
-              }
-            }
-        });
+                loadTemplate(attrs.templateUrl);
 
-        //What should happen on change
-        $(select2Element).on("change", function(e){
-            if(e.added)
-            {
-                scope.employers.push(e.added);
-            }
-            else if(e.removed)
-            {
-                var i = scope.employers.indexOf(e.removed);
-                scope.employers.splice(i,1);
-            }
+                function loadTemplate(template) {
+                    $http.get(template, { cache: $templateCache })
+                        .success(function (templateContent) {
 
-        });
-      }
-    };
-  });
+                            var container = document.createElement("div");
+                            $(container).html(templateContent);
+                            element.replaceWith(container);
+
+                            var select2Element = $(container).children().get(0);
+                            $(select2Element).select2({
+                                minimumInputLength: attrs.minimumInputLength,
+                                maximumSelectionSize: attrs.maximumSelectionSize,
+                                multiple: true,
+                                id: function (obj) {
+                                    return obj.id;
+                                },
+                                ajax: {
+                                    url: attrs.employerSearchServiceUrl,
+                                    transport: function (queryParams) {
+                                        $.ajax({
+                                            url: queryParams.url + '?q=' + queryParams.data.q,
+                                            async: false,
+                                            headers: { 'Authorization': 'Bearer ' + keycloak.token }
+                                        }).done(function (results) {
+                                            queryParams.success({results: results});
+                                        });
+
+                                    },
+                                    dataType: 'jsonp',
+                                    data: function (term, page) {
+                                        return {q: term} //search term
+                                    },
+                                    results: function (data, page, query) {
+                                        return data;
+                                    }
+                                }
+                            });
+
+                            //What should happen on change
+                            $(select2Element).on("change", function (e) {
+                                if (e.added) {
+                                    scope.employers.push(e.added);
+                                }
+                                else if (e.removed) {
+                                    var i = scope.employers.indexOf(e.removed);
+                                    scope.employers.splice(i, 1);
+                                }
+
+                            });
+
+
+
+                        });
+                }
+
+
+            }
+        };
+    });
